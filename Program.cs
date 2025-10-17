1146fbd99638939f805c0f5395bed0cd62789fae
Liam-Bowerman <liboad0@student.oaisd.org>
2025-10-17 17:58:57 +0000

    added gaps to make it look better

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ class Program
     Console.WriteLine("Insert first number:");
     String num1 = Console.ReadLine();
     number1 = double.Parse(num1);
+    Console.WriteLine("");
     Console.WriteLine("Insert second number:");
     String num2 = Console.ReadLine();
     number2 = double.Parse(num2);
@@ -19,8 +20,12 @@ class Program
         Console.Clear();
         while (true)
         {
-            Console.WriteLine("What type of problem do you want to do? To add enter a +. To subtract enter a -. To multiply enter an *. To divide with decimals enter a /. If you want to divide without decimals, enter //.  To find a remainder enter a %. If you want to do an exponent, type ^. If you would like to do fractions, enter x/x.");
+            Console.WriteLine("");
+            Console.WriteLine("What type of problem do you want to do? To add enter a +. To subtract enter a -. To multiply enter an *. To divide with decimals enter a /.");
+            Console.WriteLine("If you want to divide without decimals, enter //.  To find a remainder enter a %. If you want to do an exponent, type ^. If you would like to do fractions, enter x/x.");
+            Console.WriteLine("");
             String equationType = Console.ReadLine();
+            Console.WriteLine("");
             if (equationType == "x/x") 
             {
                 Console.WriteLine("What will you do you want to do with fractions? +, -, *, /, or ^?");
@@ -29,15 +34,19 @@ class Program
                 Console.WriteLine("Please enter first Numerator.");
                 String numerator1 = Console.ReadLine();
                 int numerator1Int = int.Parse(numerator1);
+                Console.WriteLine("");
                 Console.WriteLine("Please enter first denominator.");
                 String denominator1 = Console.ReadLine();
                 int denominator1int = int.Parse(denominator1);
+                Console.WriteLine("");
                 Console.WriteLine("Please enter second Numerator.");
                 String numerator2 = Console.ReadLine();
                 int numerator2Int = int.Parse(numerator2);
+                Console.WriteLine("");
                 Console.WriteLine("Please enter second denominator.");
                 String denominator2 = Console.ReadLine();
                 int denominator2int = int.Parse(denominator2);
+                Console.WriteLine("");
                 if (fractionEquationType == "+")
                 {
                     Console.Clear();
